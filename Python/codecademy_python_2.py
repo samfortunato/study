aaa1d3bc83dc34c24a288266be449100922999fd
Samuel Fortunato <samfortunato@gmail.com>
2019-03-17 14:43:39 -0400

    Finish class section of Codecademy course

--- Python/codecademy_python_2.py
+++ Python/codecademy_python_2.py
@@ -55,3 +55,15 @@ def flip_bit(number, nth_bit):
   flipped_bits = number ^ flip_mask
 
   return bin(flipped_bits)
+
+class Point3D(object):
+  def __init__(self, x, y, z):
+    self.x = x
+    self.y = y
+    self.z = z
+    
+  def __repr__(self):
+    return '(%d, %d, %d)' % (self.x, self.y, self.z)
+  
+my_point = Point3D(1, 2, 3)
+print(my_point)
