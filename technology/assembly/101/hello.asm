856c87e1eb756b5594c31df04d9e910d22460ca2
Samuel Fortunato <samfortunato@gmail.com>
2025-11-09 02:32:58 -0800

    try basic assembly program

--- technology/assembly/101/hello.asm
+++ technology/assembly/101/hello.asm
@@ -1,22 +1,7 @@
 .global _main
 .align 2
 
-.data
-fmt: .asciz "%d\n"
-
 .text
 _main:
-	stp x29, x30, [sp, #-16]!
-	mov x29, sp
-
-	mov w0, #5
-	add w0, w0, #3
-
-	adrp x1, fmt@PAGE
-	add x1, x1, fmt@PAGEOFF
-	mov w1, w0
-	bl _printf
-
-	mov w0, #0
-	ldp x29, x30, [sp], #16
+	mov w0, #8
 	ret
